fc2b4f45e3ee4ab9f6cedf7cc79f88d12d101207
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-16 21:24:17 +0600

    Added few lines

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 555;
+const singer = {name: 'Ali Hossain', job:'singer'};
+const singer2 = {name: 'Eva Akter', job: 'singer two'};
+
+const singerStyle ={
+  color: 'white',
+  backgroundColor: 'red',
+
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Ali" job="Teacher"></Person>
+      <Person name="Hossain" job="Driver"></Person>
+      <h2>New Component</h2>
+      <Friend phone="029992" movie="The king"></Friend>
+      <Friend phone="2239992"></Friend>
     </div>
   );
+}
+
+function Person(props){
+  // console.log(props);
+  return (
+    <div className='person'>
+      <h2>{props.name}</h2>
+      <p>Profession: {props.job}</p>
+    </div>
+  )
+}
+
+function Friend(props){
+  console.log(props);
+  return(
+    <div className='container'>
+      <h3>Name: {props.movie}</h3>
+      <p>phone:{props.phone}</p>
+    </div>
+  )
 }
 
 export default App;

ce647e5352c517009c1f5878b3c087be410e9608
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-17 02:47:05 +0600

    State, component state hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const number = 555;
 const singers = [
@@ -25,39 +26,65 @@ function App() {
   ]
   return (
     <div className="App">
-      {
-        hero.map(hero => <li>{hero}</li>)
-      }
-      {/* {
-        hero.map(hero => <Person name={hero}> </Person>)
-      } */}
-
-      {
-        singers.map(singer => <Person name={singer.name}></Person>)
-      }
-
-      {/* 
-      <Person name={hero[0]} job="Teacher"></Person>
-      <Person name={hero[1]} job="Driver"></Person> */}
-
-      <h2>New Component</h2>
-      <Friend phone="029992" movie="The king"></Friend>
-      <Friend phone="2239992"></Friend>
-
-
-      {/* ******** Products ******** */}
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-      {/* <Product name="laptop" price="20,000" ></Product>
-      <Product name="Mobile" price="2,000" ></Product>
-      <Product name="Watch" price="4,000" ></Product> */}
+      <Counert></Counert>
     </div>
   );
 }
 
+/* ******** Products ********
+{
+  products.map(product => <Product name={product.name} price={product.price}></Product>)
+}
+<Product name="laptop" price="20,000" ></Product>
+<Product name="Mobile" price="2,000" ></Product>
+<Product name="Watch" price="4,000" ></Product>
+*/
+
+
+/* 
+{
+  hero.map(hero => <li>{hero}</li>)
+}
+
+{
+  hero.map(hero => <Person name={hero}> </Person>)
+}
+
+{
+  singers.map(singer => <Person name={singer.name}></Person>)
+}
+
+<Person name={hero[0]} job="Teacher"></Person>
+<Person name={hero[1]} job="Driver"></Person>
+
+<h2>New Component</h2>
+<Friend phone="029992" movie="The king"></Friend>
+<Friend phone="2239992"></Friend>
+*/
 
 // =========== Component start from here =====================
+
+function Counert(){
+  const [count, setCount] = useState(4);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+  /* const increaseCount = () =>{
+    const newCount = count + 1;
+    setCount(newCount);
+  } */
+
+  // console.log(abc);
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increage</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
 function Person(props){
   // console.log(props);
   return (

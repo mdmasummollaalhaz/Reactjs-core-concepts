f48b441fef5b9578c6355783bcdc4c6a44aeabda
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-17 01:23:09 +0600

    components from an array

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 555;
-const singer = {name: 'Ali Hossain', job:'singer'};
-const singer2 = {name: 'Eva Akter', job: 'singer two'};
+const singers = [
+  {name: 'Ali Hossain', job:'Singer'},
+  {name: 'Eva Akter', job: 'Driver'},
+  {name: 'Akter', job: 'Car washer'},
+  {name: 'Ramjan', job:'Berber'},
+];
 
 const singerStyle ={
   color: 'white',
@@ -12,10 +16,24 @@ const singerStyle ={
 }
 
 function App() {
+  const hero =['Rubel', 'BappaRaz', 'Salman Khan', 'The king']
   return (
     <div className="App">
-      <Person name="Ali" job="Teacher"></Person>
-      <Person name="Hossain" job="Driver"></Person>
+      {
+        hero.map(hero => <li>{hero}</li>)
+      }
+      {/* {
+        hero.map(hero => <Person name={hero}> </Person>)
+      } */}
+
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+
+      {/* 
+      <Person name={hero[0]} job="Teacher"></Person>
+      <Person name={hero[1]} job="Driver"></Person> */}
+
       <h2>New Component</h2>
       <Friend phone="029992" movie="The king"></Friend>
       <Friend phone="2239992"></Friend>
@@ -28,7 +46,7 @@ function Person(props){
   return (
     <div className='person'>
       <h2>{props.name}</h2>
-      <p>Profession: {props.job}</p>
+      <p>{props.job}</p>
     </div>
   )
 }
